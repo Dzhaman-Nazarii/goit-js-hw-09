594f7fdfce9792b5039f48039faf34cda0c7f128
Dzhaman-Nazarii <dzhaman.nazar2003@gmail.com>
2023-04-13 10:52:44 +0300

    exercise 1 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const body = document.querySelector('body')
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
 
+btnStart.addEventListener("click", clickStart);
+btnStop.addEventListener("click", clickStop);
+let timerId = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+
+function clickStart() {
+    timerId = setInterval(() => {
+        let color = getRandomHexColor();
+        body.style.background = color;
+        btnStart.setAttribute("disabled", "disabled");
+        btnStop.removeAttribute("disabled");
+    },1000)
+}
+
+function clickStop() {
+    clearInterval(timerId);
+    btnStart.removeAttribute("disabled");
+    btnStop.setAttribute("disabled", "disabled");
+}
