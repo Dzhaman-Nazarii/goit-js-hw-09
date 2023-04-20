53db52bf338cec74ebeee4c9b90a97de780942dc
Dzhaman-Nazarii <dzhaman.nazar2003@gmail.com>
2023-04-20 10:17:21 +0300

    exercise 3 done

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,35 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+const form = document.querySelector(`.form`)
+form.addEventListener(`submit`, onSubmit)
+function onSubmit(evt) {
+  evt.preventDefault();
+  
+  let delay = Number(evt.currentTarget.delay.value);
+  const step = Number(evt.currentTarget.step.value);
+  const amount = Number(evt.currentTarget.amount.value);
+  
+  for (let position = 1; position <= amount; position += 1) {
+    createPromise(position, delay)
+      .then(({ position, delay }) => {
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+    delay += step;
   }
+  form.reset();
 }
+
+function createPromise(position, delay) {
+    const shouldResolve = Math.random() > 0.3;
+
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (shouldResolve) {
+          resolve({ position, delay });
+      } else {
+          reject({ position, delay });
+      }
+      }, delay)
+    });
+  }
